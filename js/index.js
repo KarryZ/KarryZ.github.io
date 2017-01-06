b2963a3c6347390e712833fa8a219ebac56a7a27
Kate <karry_ji@mail.ru>
2017-01-06 18:22:15 +0300

    add background color to mobile version nav; add social links; change contact form style;

--- js/index.js
+++ js/index.js
@@ -13,3 +13,7 @@ $(window).scroll(collapseNavbar), $(document).ready(collapseNavbar), $(function 
     "dropdown-toggle active" != $(this).attr("class") && "dropdown-toggle" != $(this).attr("class") && $(".navbar-toggle:visible").click()
 });
 
+$(".navbar-toggle").click(function () {
+     $('.navbar-custom').toggleClass("navbar-color");
+
+   });
