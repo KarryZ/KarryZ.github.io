ade8233704be31c090666f79f7f23fcda2e3d406
Kate <karry_ji@mail.ru>
2017-01-08 22:11:16 +0300

    add palindrom link;

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 function collapseNavbar() {
     $(".navbar").offset().top > 50 ? $(".navbar-fixed-top").addClass("top-nav-collapse") : $(".navbar-fixed-top").removeClass("top-nav-collapse")
 }
-
 $(window).scroll(collapseNavbar), $(document).ready(collapseNavbar), $(function () {
     $("a.page-scroll").bind("click", function (e) {
         var t = $(this);
@@ -12,8 +11,6 @@ $(window).scroll(collapseNavbar), $(document).ready(collapseNavbar), $(function 
 }), $(".navbar-collapse ul li a").click(function () {
     "dropdown-toggle active" != $(this).attr("class") && "dropdown-toggle" != $(this).attr("class") && $(".navbar-toggle:visible").click()
 });
-
 $(".navbar-toggle").click(function () {
-     $('.navbar-custom').toggleClass("navbar-color");
-
-   });
+    $('.navbar-custom').toggleClass("navbar-color");
+});
